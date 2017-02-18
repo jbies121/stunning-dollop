ab4a6cbcf99a4a5e0ddb0ceb604fafd77ccbb6a8
root <jbies121@gmail.com>
2017-02-17 21:04:27 -0600

    now with Working!

--- singlesub.py
+++ singlesub.py
@@ -1,23 +1,23 @@
 # Letter Frequency Analysis
 # abruz & jbies121
 def checkLetters(let):
-	letters = let
-	checked = []
-	exists = False
-	i=0
-	j=0
-	while i < len(letters):
-	    while j < len(checked):
-		if(letters[i] == checked[j]):
-		    exists = True
-		    j+=1
-		if exists == False:
-		    t = letters[i]
-		    checked.append(t)
-		    countLetters(letters, i)
-		    j=0
-		    i+=1
-		    exists = False
+        letters = let
+        checked = []
+        exists = False
+        i=0
+        j=0
+        while i < len(letters):
+            while j < len(checked):
+                if letters[i] == checked[j]:
+                    exists = True
+                j+=1
+            if exists == False:
+                t = letters[i]
+                checked.append(t)
+                countLetters(letters, i)
+            j=0
+            i+=1
+            exists = False
 	
 def countLetters(let, a):
 	char = let[a]
